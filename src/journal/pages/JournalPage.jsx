072185f31afcc56a2fcfd79c5c991001f8d28cfa
ThunderGer <Jaames11@hotmail.com>
2023-01-02 22:58:08 -0600

    ButtonOk

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,25 @@
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NoteView />
-      {/* <NothingSelectedView /> */}
+      {/* <NoteView /> */}
+      <NothingSelectedView />
+
+      <IconButton
+        size='large'
+        sx= {{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {backgroundColor: 'error.main', opacity: 0.8},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}>
+          <AddOutlined sx = {{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
